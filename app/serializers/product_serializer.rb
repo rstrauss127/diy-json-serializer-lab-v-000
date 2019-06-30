c38c841b51e358d3aad2b1ed58de90cea5b7023a
Rebecca Strauss <rstrauss127@gmail.com>
2019-06-30 21:07:19 +0000

    Automatically backed up by Learn

--- app/serializers/product_serializer.rb
+++ app/serializers/product_serializer.rb
@@ -3,12 +3,12 @@ class ProductSerializer
     #start with open brace to create valid JSON object
     serialized_product = '{'
 
-    serialized_product += '"id": ' + product.id.to_s + ', '
-    serialized_product += '"name": "' + product.name + '", '
-    serialized_product += '"description": "' + product.description.to_s + '", '
-    serialized_product += '"price": "' + product.price.to_s + '", '
-    serialized_product += '"inventory": ' + product.inventory.to_i
-    #end with close bracket
-    serialized_product += '}'
+  serialized_product += '"id": ' + product.id.to_s + ', '
+  serialized_product += '"name": "' + product.name + '", '
+  serialized_product += '"price": ' + product.price.to_s + ', '
+  serialized_product += '"inventory": ' + product.inventory.to_s + ', '
+  serialized_product += '"description": "' + product.description + '" '
+
+  serialized_product += '}'
   end
 end
