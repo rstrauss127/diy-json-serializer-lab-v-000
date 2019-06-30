7f9891ccc7683434ca232f360199b2d2dd4e2042
Rebecca Strauss <rstrauss127@gmail.com>
2019-06-30 20:55:26 +0000

    Automatically backed up by Learn

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -10,7 +10,7 @@ class ProductsController < ApplicationController
 
   def description
     product = Product.find(params[:id])
-    render plain: product.description
+    render plain: product.description.to_s
   end
 
   def new
