133501b5d61925192a446c0965e7e6bfd05ccd40
Rebecca Strauss <rstrauss127@gmail.com>
2019-06-30 20:05:26 +0000

    Automatically backed up by Learn

--- app/serializers/product_serializer.rb
+++ app/serializers/product_serializer.rb
@@ -6,6 +6,8 @@ class ProductSerializer
     serialized_product += '"id": ' + product.id.to_s + ', '
     serialized_product += '"name": "' + product.name + '", '
     serialized_product += '"description": "' + product.description + '", '
+    serialized_product += '"price": "' + product.price.to_s + '", '
+    serialized_product += '"inventory": ' + product.inventory.to_s
     #end with close bracket
     serialized_product += '}'
   end
